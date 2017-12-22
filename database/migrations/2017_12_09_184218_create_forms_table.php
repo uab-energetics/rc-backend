3ee2131b6d69fd05b1c11b087081692ce7f46ca3
Caleb Falcione <caleb.falcione@gmail.com>
2017-12-22 16:23:15 -0600

    Revised form deletion strategy

--- database/migrations/2017_12_09_184218_create_forms_table.php
+++ database/migrations/2017_12_09_184218_create_forms_table.php
@@ -4,26 +4,27 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFormsTable extends Migration
-{
+class CreateFormsTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('forms', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('root_category_id');
+            $table->unsignedInteger('root_category_id')->nullable();
 
             $table->string('type');
             $table->string('name');
             $table->string('desc')->nullable();
             $table->boolean('published')->default(false);
 
-            $table->foreign('root_category_id')->references('id')->on('categories');
+            $table->foreign('root_category_id')->references('id')->on('categories')
+                ->onDelete('set null');
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -32,8 +33,7 @@ class CreateFormsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('forms');
     }
 }
