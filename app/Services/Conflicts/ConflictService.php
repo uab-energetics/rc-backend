3ab6a668f36a867ec893a14acac55f9646814412
Chris Rocco <chris.rocco7@gmail.com>
2017-12-23 02:42:06 -0600

    more meat

--- app/Services/Conflicts/ConflictService.php
+++ app/Services/Conflicts/ConflictService.php
@@ -21,26 +21,47 @@ class ConflictService {
         $other_encodings = $form->encodings()->where('id', '!=', $encoding_id)->get();
         $questions = $form->questions;
 
+        $conflict_records = [];
         foreach($other_encodings as $other_encoding){
             foreach($questions as $question){
-                $my_answer = $encoding->lookupAnswer($question);
-                $their_answer = $other_encoding->lookupAnswer($question);
+                $my_answer = $this->lookupResponse($encoding, $question);
+                $their_answer = $this->lookupResponse($other_encoding, $question);
                 if($my_answer && $their_answer){
-                    $result = $this->compareAnswers($my_answer, $their_answer);
-                    if(!$result){
-                        // handle disagree
-                    } else {
-                        // handle agree
+                    $doesAgree = $this->compareAnswers($my_answer, $their_answer);
+                    $status = true;
+                    $message = '';
+                    if(!$doesAgree){
+                        $status = false;
+                        $message = $this->fail_message;
                     }
+                    $conflict_records[] = [ // that push syntax though
+                        'user_encoding_id' => $encoding_id,
+                        'other_encoding_id' => $other_encoding->id,
+                        'question' => $question->id,
+                        'status' => $status,
+                        'msg' => $message
+                    ];
+                    $this->fail_message = '';
                 }
             }
         }
+        // save all conflict records in DB in one query.
     }
 
+    /**
+     * Compares two responses for agreement.
+     * May set a descriptive message on disagreement.
+     * @param $my_response
+     * @param $their_response
+     * @return bool
+     */
     function compareAnswers($my_response, $their_response){
         $this->fail_message = "always disagree";
         return false;
-//        return $my_response['type'] === $their_response['type'];
+    }
+
+    protected function lookupResponse($encoding, $question){
+        return '';
     }
 
 }
