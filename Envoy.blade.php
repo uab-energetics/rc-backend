418688ee4bbcd2b1a1d6b52bb34191b1e46f83d5
Chris Rocco <chris.rocco7@gmail.com>
2017-12-24 16:24:15 -0600

    slack hook in envoy deployment script

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -6,3 +6,7 @@
     composer install
     php artisan migrate
 @endtask
+
+@finished
+    @slack("https://hooks.slack.com/services/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", "#notifications", "An update to research coder API has been published!");
+@endfinished
