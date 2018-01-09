b172290c7b62b3e81530186a3ebb53c46367b759
Caleb Falcione <caleb.falcione@gmail.com>
2018-01-09 15:44:02 -0600

    Added description to categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['parent_id', 'name'];
+    protected $fillable = ['parent_id', 'name', 'description'];
     protected $with = ['children', 'questions'];
 
     function children(){
