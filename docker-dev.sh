ef7cc891afef427cb5c48240a1478748e48feffe
Caleb Falcione <caleb.falcione@gmail.com>
2018-01-21 19:47:57 -0600

    Many dockerization fixes and improvements

--- docker-dev.sh
+++ docker-dev.sh
@@ -2,18 +2,9 @@
 
 COMPOSE_FILE=docker-compose.dev.yml
 
-# handle --down switch
-if [ "$1" = "--down" ]; then {
-  shift 1
-  docker-compose -f $COMPOSE_FILE down $@
-  exit 0
-}; fi
-
 ### Declare functions for later use
 
-run() {
-   docker-compose -f $COMPOSE_FILE up $@
-}
+alias run="docker-compose -f $COMPOSE_FILE up"
 
 migrate() {
   docker exec rc-api-dev php artisan migrate -n --seed
@@ -32,17 +23,21 @@ initialize() {
     echo initialized vendor folder
   }; fi
 
-  # ensure proper permissions
-  chmod -R g+r . && \
-  chmod -R g+w storage/ bootstrap/cache/ && \
-  echo set permissions
-
   # handle the --migrate switch
   if [ "$1" = "--migrate" ]; then {
     shift 1 #not currently used but safe to have
     migrate && \
     echo migrated and seeded database
   }; fi
+
+  # ensure proper permissions
+  docker exec rc-api-dev chown -R --reference=. . 
+  docker exec rc-api-dev usermod -u $USER_UID www-data 
+  docker exec rc-api-dev groupmod -g $USER_GID www-data
+  chmod -R 755 . && \
+  chmod -R 775 storage/ bootstrap/cache/ && \
+  echo set permissions
+
 }
 
 # ensure that pwd is the root of our project
@@ -62,7 +57,14 @@ if [ $? = 2 ]; then {
   echo created environment file
 }; fi
 
+# handle --down switch
+if [ "$1" = "--down" ]; then {
+  shift 1
+  docker-compose -f $COMPOSE_FILE down $@
+  exit 0
+}; fi
 
-run --build -d $(echo $@ | sed s/-d// | sed s/--migrate// )
+run -d $(echo $@ | sed s/-d// | sed s/--migrate// )
 initialize $@
+docker stop rc-api-dev rc-db-dev
 run $( echo $@ | sed s/--migrate// )
