ac41606e8ba4cb214caf43adc6bd2e2a5ccf56d6
Caleb <caleb.falcione@gmail.com>
2018-02-15 20:50:05 -0600

    Created migration for publication source ids

--- app/Models/Publication.php
+++ app/Models/Publication.php
@@ -10,8 +10,8 @@ class Publication extends Model {
 
     protected $table = 'publications';
 
-    protected $fillable = ['name', 'embedding_url'];
+    protected $fillable = ['name', 'source_id', 'embedding_url'];
 
-    const searchable = ['name', 'embedding_url'];
+    const searchable = ['name', 'source_id', 'embedding_url'];
 
 }
