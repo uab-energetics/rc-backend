a1d8affe11cc08a255af1ac6f20476ca0a537e4b
Chris Rocco <chris.rocco7@gmail.com>
2018-05-18 18:58:18 -0500

    added test cases

---
+++ tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+
+// DO NOT DELETE - this file is required
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+}
