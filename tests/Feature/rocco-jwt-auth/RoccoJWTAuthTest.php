a5da1dd7e79569ce9b3effea37af299b57821a67
Caleb <caleb.falcione@gmail.com>
2018-05-22 16:16:32 -0500

    renames RoccoJWTAuth to RoccoJWTAuthTest

--- tests/Feature/rocco-jwt-auth/RoccoJWTAuthTest.php
+++ tests/Feature/rocco-jwt-auth/RoccoJWTAuthTest.php
@@ -8,7 +8,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class RoccoJWTAuth extends TestCase
+class RoccoJWTAuthTest extends TestCase
 {
     public function testJWTRoute() {
 
