c90a591665dcfe814a2b04c944b3a698f6843a2a
Caleb <caleb.falcione@gmail.com>
2018-05-22 10:28:41 -0500

    changes some event declarations to use class references instead of hard-coded strings

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,34 +2,37 @@
 
 namespace App\Providers;
 
+use App\Events\CommentUpdate;
+use App\Events\EncodingChanged;
 use App\Events\EncodingCreated;
 use App\Events\FormDeleted;
 use App\Events\FormQuestionRemoved;
 use App\Events\UserCreated;
 use App\Events\UserDeleted;
 use App\Events\UserUpdated;
+use App\Listeners\CommentsListener;
 use App\Listeners\EncodingCreatedListener;
 use App\Listeners\FormDeletedListener;
 use App\Listeners\FormQuestionRemovedListener;
+use App\Listeners\RunConflictScan;
 use App\Listeners\UserCreatedListener;
 use App\Listeners\UserDeletedListener;
 use App\Listeners\UserUpdatedListener;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
-class EventServiceProvider extends ServiceProvider
-{
+class EventServiceProvider extends ServiceProvider {
     /**
      * The event listener mappings for the application.
      *
      * @var array
      */
     protected $listen = [
-        'App\Events\EncodingChanged' => [
-            'App\Listeners\RunConflictScan',
+        EncodingChanged::class => [
+            RunConflictScan::class,
         ],
-        'App\Events\CommentUpdate' => [
-            'App\Listeners\CommentsListener'
+        CommentUpdate::class => [
+            CommentsListener::class,
         ],
         FormQuestionRemoved::class => [
             FormQuestionRemovedListener::class,
@@ -56,8 +59,7 @@ class EventServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot() {
         parent::boot();
 
         //
