ebc367b89826874c4568529bb487768af6eb3382
Caleb <caleb.falcione@gmail.com>
2018-05-22 16:24:54 -0500

    adds uuid to UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,6 +18,7 @@ $factory->define(App\User::class, function (Faker $faker) {
 
     return [
         'name' => $faker->name,
+        'uuid' => $faker->unique()->randomAscii,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
