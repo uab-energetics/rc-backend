b1b9e9b82cbb7be6ac9d251f6b41f4f8f700983f
Caleb <caleb.falcione@gmail.com>
2018-05-29 16:02:05 -0500

    updates line to use bracket array syntax

--- app/Messaging/RabbitConsumer.php
+++ app/Messaging/RabbitConsumer.php
@@ -14,7 +14,7 @@ class RabbitConsumer {
 
     public function registerListener($exchange, $queue, $callback) {
         $this->channel->exchange_declare($exchange, 'fanout', false, false, false);
-        list($queue_name, , ) = $this->channel->queue_declare($queue, false, false, true, false);
+        [$queue_name, , ] = $this->channel->queue_declare($queue, false, false, true, false);
         $this->channel->queue_bind($queue_name, $exchange);
         $this->channel->basic_consume($queue_name, '', false, false, false, false, function($msg) use ($callback) {
             $data = json_decode($msg->body, true);
