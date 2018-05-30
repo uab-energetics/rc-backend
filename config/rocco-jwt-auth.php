33626aaf84c45f4580f2ffe7b73ba61f9cc9594a
Caleb <caleb.falcione@gmail.com>
2018-05-30 12:41:01 -0500

    fixes jwt config

--- config/rocco-jwt-auth.php
+++ config/rocco-jwt-auth.php
@@ -4,7 +4,7 @@ return [
     'public_key' => file_get_contents(env('JWT_PUBLIC_KEY')),
     'algorithm' => env('JWT_ALGORITHM', 'RS256'),
     'user_id_field' => env('USER_UUID_COLUMN', 'uuid'), // uses subject of JWT
-    'jwt_user_id' => 'uuid',
+    'jwt_user_id' => '_id',
 
     'no-jwt-middleware' => env('NO_JWT_MIDDLEWARE', false)
 ];
