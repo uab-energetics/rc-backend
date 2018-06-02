77ab45bf1f1a99387e73cd177562b9b5c0b611ba
Caleb <caleb.falcione@gmail.com>
2018-06-02 12:32:23 -0500

    make rocco-jwt-auth config use default jwt public key

--- config/rocco-jwt-auth.php
+++ config/rocco-jwt-auth.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'public_key' => file_get_contents(env('JWT_PUBLIC_KEY')),
+    'public_key' => file_get_contents(env('JWT_PUBLIC_KEY', '.keys/jwt.pub')),
     'algorithm' => env('JWT_ALGORITHM', 'RS256'),
     'user_id_field' => env('USER_UUID_COLUMN', 'uuid'), // uses subject of JWT
     'jwt_user_id' => 'user._id',
